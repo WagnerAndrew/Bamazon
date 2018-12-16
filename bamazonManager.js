f6bb362218afad7a81698ef3c97df9437f29da30
WagnerAndy <wagner.andrew.james@gmail.com>
2018-12-15 23:22:23 -0700

    Manager complete

--- bamazonManager.js
+++ bamazonManager.js
@@ -83,9 +83,6 @@ function viewProducts() {
 
 }
 
-// SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1
-// select * from users where length(zip_code) < 5
-
 function lowInventory() {
     connection.query("SELECT * FROM products WHERE (Quantity) < 5", function (err, res) {
         if (err) throw err;
@@ -99,3 +96,120 @@ function lowInventory() {
     });
 
 }
+
+function addToInventory() {
+
+    connection.query("SELECT ID, Product, Price, Quantity FROM products", function (err, res) {
+        if (err) throw err;
+
+        //Prints Table
+        console.log("\n");
+            console.table(res);
+
+        //Runs purchase inquiry
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "id",
+                message: "What ID number do you want to add inventory to?"
+            },
+
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many do you want to add"
+            }
+
+        ]).then(function (add) {
+
+            connection.query("SELECT * FROM products WHERE ?", { ID: add.id }, function (err, res) {
+                if (err) throw err;
+                
+                    var quantityUpdate = (parseInt(res[0].Quantity) + parseInt(add.quantity));
+                    
+                    connection.query("UPDATE products SET ? WHERE ?", [{ Quantity: quantityUpdate }, { ID: add.id }], function (err, res) {
+                        if (err) throw err;
+                        
+                    });
+
+                        connection.query("SELECT ID, Product, Price, Quantity FROM products", function (err, res) {
+                            if (err) throw err;
+                            
+                            console.log("\n***************************\nYou added " + add.quantity + " to " + res[0].Product + "\n");
+                            console.log("There are now " + res[0].Quantity + " " + res[0].Product + "\n***************************\n");
+
+                            userSearch();
+
+                        });
+
+            });
+        });
+    });
+}
+
+
+function addNewProduct() {
+
+    connection.query("SELECT ID, Product, Price, Quantity FROM products", function (err, res) {
+        if (err) throw err;
+
+        //Prints Table
+        console.log("\n");
+            console.table(res);
+
+        //Runs purchase inquiry
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "product",
+                message: "What product do you want to add?"
+            },
+            {
+                type: "input",
+                name: "department",
+                message: "What department category does it belong in?"
+            },
+            {
+                type: "input",
+                name: "price",
+                message: "What is the price?"
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many do you want to add?"
+            },
+
+
+        ]).then(function (user) {
+
+            connection.query ("INSERT INTO products SET ?",
+            {
+              Product: user.product,
+              Department: user.department,
+              Price: parseInt(user.price),
+              Quantity: parseInt(user.quantity)
+            },
+        
+            function (err, res) {
+              if (err) throw err;
+
+            });
+            // SELECT fields FROM table ORDER BY id DESC LIMIT 1;
+            connection.query("SELECT Product, Price, Quantity FROM products ORDER BY ID DESC LIMIT 1", function (err, res) {
+                if (err) throw err;
+
+              console.log("You added: \n");
+                console.table(res);
+
+                userSearch();
+
+            });
+
+}); 
+})   
+};                   
+
+
+
+
